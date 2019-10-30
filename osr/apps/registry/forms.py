c74f5de729c9d30ec935cdc3a2057792739a8fd0
Владимир Н. Киселев <KiselevVN@rsue.ru>
2019-10-30 13:46:45 +0300

    fix bug

--- osr/apps/registry/forms.py
+++ osr/apps/registry/forms.py
@@ -19,7 +19,6 @@ class ParentForm(forms.ModelForm):
 class SportsmanForm(forms.ModelForm):
     class Meta:
         model = models.Sportsman
-<<<<<<< HEAD
         fields = ('__all__')
 
 class UMOForm(forms.ModelForm):
@@ -31,22 +30,3 @@ class PrimaryForm(forms.ModelForm):
     class Meta:
         model = models.Primary
         fields = ('__all__')
-=======
-        fields = [
-            'name',
-            'surname',
-            'patronymic',
-            'date_of_birth',
-            'gender',
-            'location',
-            'telephone',
-            'sports_facility',
-            'swimming_skills',
-            'school_progress',
-            'sport_desire',
-            'coach',
-            'parent',
-            'sport_type',
-            'rank',
-        ]
->>>>>>> 477aaca558b6b5350d46ae44d7a384fca1c7d27c
