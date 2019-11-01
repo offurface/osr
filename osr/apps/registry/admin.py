fd4907e92e034ab4a25165f73f271552942ca337
Владимир Н. Киселев <KiselevVN@rsue.ru>
2019-11-01 16:19:40 +0300

    правки

--- osr/apps/registry/admin.py
+++ osr/apps/registry/admin.py
@@ -4,7 +4,7 @@ from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
-@admin.register(Coach, Sportsman, Parent, UMO, Rank, Sport_type, Survey, Primary)
+@admin.register(Coach, Sportsman, Parent, UMO, Rank, Sport_type, Primary)
 class ViewAdmin(ImportExportModelAdmin):
-    pass
+    to_encoding= 'utf-8'
 
