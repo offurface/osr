022384b6b4eb72716b398fe69a703c071a49bc44
Владимир Н. Киселев <KiselevVN@rsue.ru>
2019-11-12 12:58:04 +0300

    fix bug

--- osr/apps/registry/models/objects.py
+++ osr/apps/registry/models/objects.py
@@ -6,13 +6,6 @@
     1-ий
     2-ий
     3-ий
-
-    STAGE = (
-    #('первичный', 'первичный'), > UMO
-    ('1 этап', '1 этап'),
-    ('2 этап', '2 этап'),
-    ('3 этап', '3 этап'),
-)
 вид спорта > Этап > Спортсмены
 
 """
@@ -271,15 +264,15 @@ class Survey(models.Model):
     close_eyes = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
     verbose_name="Закрытые глаза")
     target = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="Мишень")
+    verbose_name="Мишень")##############
     kg = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="PWC Кг мм")
+    verbose_name="PWC Кг мм")#
     chss = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="PWC ЧСС")
+    verbose_name="PWC ЧСС")#
     arterial_pressure_f = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="Артериальное давление(верхнее число)") # Вверхнее число
+    verbose_name="Артериальное давление(верхнее)")
     arterial_pressure_s = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="Артериальное давление(нижнее число)") # Нижнее число
+    verbose_name="Артериальное давление(нижнее)")
     mpc = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
     verbose_name="МПК мл/мин/кг")
 
@@ -294,51 +287,51 @@ class Survey(models.Model):
 
 class Primary(Survey):
     past_diseases = models.CharField(max_length=250, blank=True, null=True,
-    verbose_name="Перенесенные заболевания(травмы)")
+    verbose_name="Перенесенные заболевания(травмы)")#
     height_father = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="Рост отца")
+    verbose_name="Рост отца")#
     weight_father = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="Вес отца")
+    verbose_name="Вес отца")#
     body_type_father = models.CharField(max_length=10, choices=BODY_TYPE, blank=True, null=True,
-    verbose_name="Тип тела отца")
+    verbose_name="Тип тела отца")#
     height_mother = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="Рост матери")
+    verbose_name="Рост матери")#
     weight_mother = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="Вес матери")
+    verbose_name="Вес матери")#
     body_type_mother = models.CharField(max_length=10, choices=BODY_TYPE, blank=True, null=True,
-    verbose_name="Тип тела матери")
+    verbose_name="Тип тела матери")#
     chest_shape = models.CharField(max_length=15, choices=CHEST_SHAPE, blank=True, null=True,
-    verbose_name="Форма грудной клетки")
+    verbose_name="Форма грудной клетки")#
     back_shape = models.CharField(max_length=20, choices=BACK_SHAPE, blank=True, null=True,
-    verbose_name="Форма спины")
+    verbose_name="Форма спины")#
     speed = models.PositiveIntegerField(blank=True, null=True,
-    verbose_name="Быстрота")
+    verbose_name="Быстрота")#
     strength = models.PositiveIntegerField( blank=True, null=True,
-    verbose_name="Сила%")
+    verbose_name="Сила%")#
     stamina = models.PositiveIntegerField( blank=True, null=True,
-    verbose_name="Выносливость%")
+    verbose_name="Выносливость%")#
     coordination = models.PositiveIntegerField(blank=True, null=True,
-    verbose_name="Координация%")
+    verbose_name="Координация%")#
     nervous_system_potential = models.CharField(max_length=15, choices=NERVOUS_SYSTEM_POTENTIAL, default='Средняя', blank=True, null=True,
-    verbose_name="Потенциал нервной системы")
+    verbose_name="Потенциал нервной системы")# псих тест
     run_30 = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="Бег 30м")
+    verbose_name="Бег 30м")#
     jump_place = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True,
-    verbose_name="Прыжок в длину с места")
+    verbose_name="Прыжок в длину с места")#
     pull_ups = models.PositiveIntegerField(blank=True, null=True,
-    verbose_name="Подтягивания на перекладине")
+    verbose_name="Подтягивания на перекладине")#
     concept = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True,
-    verbose_name="Концепт(техническая гребля)")
+    verbose_name="Концепт(техническая гребля)")#
     run_1500 = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True,
-    verbose_name="Бег 1500м")
+    verbose_name="Бег 1500м")#
     concept_500 = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True,
-    verbose_name="Концепт 500м")
+    verbose_name="Концепт 500м")#
     motivation = models.CharField(max_length=15, choices=MOTIVATION, blank=True, null=True,
     verbose_name="Мотивация к тренеровочному процессу")  # псих тест
     willed_qualities = models.CharField(max_length=15, choices=WILLED_QUALITIES, blank=True, null=True,
     verbose_name="Развитие волевых качеств")  # псих тест
     recommendations = models.ManyToManyField(Sport_type, blank=True, null=True,
-    verbose_name="Рекомендации")
+    verbose_name="Рекомендации")#####
 
     def __str__(self):
         return "%s %s" % (self.date_of_test, self.sportsman)
